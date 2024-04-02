3c1d65896fe424673603451f95e238f06954da6e
Bucky-26 <adonissanjuan123@gmail.com>
2024-04-03 02:17:07 +0800

    Update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const axios = require("axios").default;
 
 const app = express();
-const port = 3000;
+const port = 11000;
 
 // Middleware
 app.use(bodyParser.json());
@@ -11,47 +11,46 @@ app.use(bodyParser.json());
 app.get("/api/conversation", async (req, res) => {
   try {
     const q = req.query.q;
-    var options = {
-      method: "POST",
-      url: "https://chat.oaifree.com/backend-api/conversation",
-      headers: {
-        "Content-Type": "application/json",
-        "User-Agent": "insomnia/8.5.1",
-        "X-Authorization": "Bearer lJ1es8rPG_YetKk77G17O0vlDUPfGAXFn__HckAD0tM",
-        Cookie:
-          "_Secure-next-auth.apps-origin=https://chat.oaifree.com; google-analytics_v4_PnTc__ga4sid=1563106130; google-analytics_v4_PnTc__session_counter=1; google-analytics_v4_PnTc__ga4=32e8a6bb-59e7-4c08-b552-f3e2feb305f1; cf_chl_3=22e504c37e87f86; cf_clearance=HYIEkBpsV.7orPHM6b650op5btLL79uGMrvMlX71LyA-1705610588-1-AYQmD7/UWhmwp3uIiJiGdhHUq77MxWJaMG234C9YksXiGeuCUN94cg7k9ij09hUXwpM6Q73CzFa0BYoy7dxvIhI=; _Secure-next-auth.session-id=Ay3DE3ib4g5dGgiBD6hzWbf3HWWRFpI2LO8ji9jlAk0; google-analytics_v4_PnTc__engagementDuration=0; google-analytics_v4_PnTc__engagementStart=1705610604864; google-analytics_v4_PnTc__counter=5; google-analytics_v4_PnTc__let=1705610604864; _Secure-next-auth.session-data=MTcwNTYxMDYwNHwzUFpCaVYwdTJDYWdUT2JhZjBrZHUtcm9ZaFpudUpFeVhtcHE5MlpaQlVOYWEwRVdQYTM1N242RTFfTHV4Q0FNUUpRczhtenJkY1JMa0dOZ085anFEVEZtM0Y3STZRMWx8V06hu86R1AzLYy2D0FyyWpBt0WS3o7JAa5TSGVBkoQM=; _dd_s=rum=0&expire=1705611516326",
-      },
-      data: {
-        action: "next",
-        messages: [
-          {
-            id: "aaa28538-8a7d-4b2b-b813-40c26a6b72d0",
-            author: { role: "user" },
-            content: {
-              content_type: "text",
-              parts: [q],
-            },
-            metadata: {},
-          },
-        ],
-        parent_message_id: "aaa1cae6-a010-48ab-85c1-3984ae23b830",
-        model: "text-davinci-002-render-sha",
-        timezone_offset_min: -480,
-        suggestions: [
-          "Tell me a random fun fact about the Roman Empire",
-          "Write a short-and-sweet text message inviting my neighbor to a barbecue.",
-          "Can you suggest fun activities for a family of 4 to do indoors on a rainy day?",
-          "Show me a code snippet of a website's sticky header in CSS and JavaScript.",
-        ],
-        history_and_training_disabled: false,
-        arkose_token: null,
-        conversation_mode: { kind: "primary_assistant" },
-        force_paragen: false,
-        force_rate_limit: false,
-      },
-    };
+
+var options = {
+  method: 'POST',
+  url: 'https://chat.oaifree.com/backend-api/conversation',
+  headers: {
+    'Content-Type': 'application/json',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+    Cookie: '_Secure-next-auth.apps-origin=https://chat.oaifree.com; google-analytics_v4_PnTc__ga4=823fd194-64ea-434b-a90c-ad0e5a504244; _Secure-next-auth.session-id=hS34E72AJuDr4eGIyNVMNg80Y6Ie9VQGzexUPreF6ws; google-analytics_v4_PnTc__engagementDuration=0; google-analytics_v4_PnTc__counter=29; google-analytics_v4_PnTc__ga4sid=1749964907; google-analytics_v4_PnTc__session_counter=5; google-analytics_v4_PnTc__let=1706525535628; _Secure-next-auth.session-data=MTcwNjUyNTUzNXwwWHVXMkV0YUlMOXJfV21rZzZBYncwajh0UDJ2UXZaTmd1YmNubTNTcXhPUnFDcWkzbTRQaHFmUEFEdzBBMFlVVUgxMmNHSjdtZTIxXzc1NEsydUZ4RDhZSWwzZFkwOGp8xK6rSlPwsQhgw2H-brsRs_UtNDGO0mRF0vHGhEEOwqI=; cf_clearance=grTuciuoS0mH1TmXm5MSmSwAfsEMRFMD1DNqge5k1zE-1706525536-1-AXsVxS7MGDzHWbElJCdOyZFKnDlAeUvC1Ac9L4xuaUh46E7zuhEQveuNnMV3ZNuXqlxvgaRZixWjCDqh/FMuY00=; google-analytics_v4_PnTc__engagementStart=1706525550431; _dd_s=rum=0&expire=1706526463590',
+    'X-Authorization': 'Bearer oEfET3WA-75vsDFuRzE1L6E4qBPCfg9UCIrorg39Npc'
+  },
+  data: {
+    action: 'next',
+    messages: [
+      {
+        id: 'aaa2f1fb-b0aa-4e78-91dc-8b25726741c0',
+        author: {role: 'user'},
+        content: {content_type: 'text', parts: [q]},
+        metadata: {}
+      }
+    ],
+    parent_message_id: 'aaa1a4c7-3ba7-40ab-9dd7-da68fe4e798e',
+    model: 'text-davinci-002-render-sha',
+    timezone_offset_min: -480,
+    suggestions: [
+      'I\'m going to cook for my date who claims to be a picky eater. Can you recommend me a dish that\'s easy to cook?',
+      'Compare storytelling techniques in novels and in films in a concise table across different aspects',
+      'Plan a 3-day trip to see the northern lights in Norway. Also recommend any ideal dates.',
+      'Give me 3 ideas about how to plan good New Years resolutions. Give me some that are personal, family, and professionally-oriented.'
+    ],
+    history_and_training_disabled: false,
+    arkose_token: null,
+    conversation_mode: {kind: 'primary_assistant'},
+    force_paragen: false,
+    force_rate_limit: false
+  }
+};
+
 
     var validResponses = [];
+var id;
 
     axios
       .request(options)
@@ -74,6 +73,8 @@ app.get("/api/conversation", async (req, res) => {
                 data.message.content.parts
               ) {
                 const content = data.message.content.parts[0];
+            const iid = data.conversation_id;
+              id = iid; // Assign the value to id
 
                 if (content.trim() !== "") {
                   validResponses.push(content);
@@ -86,6 +87,7 @@ app.get("/api/conversation", async (req, res) => {
         const l = validResponses.length;
         const content = validResponses[l - 1];
         res.json({ content });
+        deletemessage(id);
       })
       .catch(function (error) {
         console.error(error);
@@ -99,3 +101,31 @@ app.get("/api/conversation", async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+
+
+
+
+async function  deletemessage(id){
+  var options = {
+    method: "PATCH",
+    url: "https://chat.oaifree.com/backend-api/conversation/" + id,
+  headers: {
+    'Content-Type': 'application/json',
+    'User-Agent': 'insomnia/8.6.0',
+    Cookie: '_Secure-next-auth.apps-origin=https://chat.oaifree.com; google-analytics_v4_PnTc__ga4=823fd194-64ea-434b-a90c-ad0e5a504244; _Secure-next-auth.session-id=hS34E72AJuDr4eGIyNVMNg80Y6Ie9VQGzexUPreF6ws; google-analytics_v4_PnTc__ga4sid=1749964907; google-analytics_v4_PnTc__session_counter=5; cf_clearance=grTuciuoS0mH1TmXm5MSmSwAfsEMRFMD1DNqge5k1zE-1706525536-1-AXsVxS7MGDzHWbElJCdOyZFKnDlAeUvC1Ac9L4xuaUh46E7zuhEQveuNnMV3ZNuXqlxvgaRZixWjCDqh/FMuY00=; google-analytics_v4_PnTc__engagementDuration=0; _Secure-next-auth.session-data=MTcwNjUyNjIyNXxPOWY1cWd2VWsycS0xcVFQMUxkOTdlVTJ2bnhvV2RTVHRENTJHaEhrbmtzd3p3cS11RzlxRnlRYjNjUnpoQndBRHpnWG5MMlJ5bWdacVBnRnFNTnJGSHdyOUJWSzhBMmR8MigLGbXylhgZ6a1l2nr3lg7mpKTAwQVD3PUU0O0DL0U=; google-analytics_v4_PnTc__engagementStart=1706526338152; google-analytics_v4_PnTc__counter=52; google-analytics_v4_PnTc__let=1706526338152; _dd_s=rum=0&expire=1706527254990',
+    'X-Authorization': 'Bearer oEfET3WA-75vsDFuRzE1L6E4qBPCfg9UCIrorg39Npc'
+  },
+    data: { is_visible: false },
+  };
+
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
